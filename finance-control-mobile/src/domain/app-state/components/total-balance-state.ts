29e539ca15be5125aa395783cab5e120acdba2e6
Антон Гаврилов <gavor0120@gmail.com>
2023-03-26 01:42:41 +0300

    Remove actual balance from app state

--- finance-control-mobile/src/domain/app-state/components/total-balance-state.ts
+++ finance-control-mobile/src/domain/app-state/components/total-balance-state.ts
@@ -6,11 +6,6 @@ type TotalBalanceState = {
    * Initial balance for billing period
    */
   initialBalance: number | null;
-
-  /**
-   * The amount of the current total balance
-   */
-  actualBalance: number;
 }
 
 export default TotalBalanceState;
--- finance-control-mobile/src/state/total-balance/actions.ts
+++ finance-control-mobile/src/state/total-balance/actions.ts
@@ -1,22 +1,10 @@
 import { Action } from "state/types";
 
 export enum TotalBalanceActionsTypes {
-  SetActualBalance = "SET_ACTUAL_BALANCE",
   SetInitialBalance = "SET_INITIAL_BALANCE"
 }
 
 export type TotalBalanceActionsReturnTypes = number;
-
-/**
- * Set new value of actual balance
- * @param amount New actual balance value
- */
-export function setCurrentBalance(amount: number): Action<TotalBalanceActionsReturnTypes> {
-  return {
-    type: TotalBalanceActionsTypes.SetActualBalance,
-    payload: amount
-  }
-}
 
 /**
  * Set new value of initial balance for billing period
