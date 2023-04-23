6e96a01c0482bd64f641127d703de23862c1f04b
Антон Гаврилов <gavor0120@gmail.com>
2023-04-23 12:52:36 +0400

    ios native app initial commit. gitignore fixes

---
+++ profitocracy-ios/Profitocracy/Profitocracy/ProfitocracyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProfitocracyApp.swift
+//  Profitocracy
+//
+//  Created by Anton Gavrilov on 23.04.23.
+//
+
+import SwiftUI
+
+@main
+struct ProfitocracyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
