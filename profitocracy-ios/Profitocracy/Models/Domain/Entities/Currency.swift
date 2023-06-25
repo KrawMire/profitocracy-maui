6cc1914966a45a6a31956aedd6b5238660e7b0fa
Антон Гаврилов <gavor0120@gmail.com>
2023-06-25 04:21:10 +0400

    native ios. added new currencies

--- profitocracy-ios/Profitocracy/Models/Domain/Entities/Currency.swift
+++ profitocracy-ios/Profitocracy/Models/Domain/Entities/Currency.swift
@@ -25,6 +25,8 @@ extension Currency {
     static let availableCurrencies: [Currency] = [
         Currency(name: "US Dollar", code: "USD", symbol: "$"),
         Currency(name: "Russian Ruble", code: "RUB", symbol: "₽"),
-        Currency(name: "European Euro", code: "EUR", symbol: "€")
+        Currency(name: "European Euro", code: "EUR", symbol: "€"),
+        Currency(name: "Armenian Dram", code: "AMD", symbol: "֏"),
+        Currency(name: "Georgian Lari", code: "GEL", symbol: "₾")
     ]
 }
