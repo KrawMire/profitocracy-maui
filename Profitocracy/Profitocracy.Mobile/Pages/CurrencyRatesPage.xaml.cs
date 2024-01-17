3be096bdf390baa02a54963aa71ce65a6e2c58b5
Антон Гаврилов <gavor0120@gmail.com>
2024-01-18 00:58:05 +0100

    ui updates

--- Profitocracy/Profitocracy.Mobile/Pages/CurrencyRatesPage.xaml.cs
+++ Profitocracy/Profitocracy.Mobile/Pages/CurrencyRatesPage.xaml.cs
@@ -2,8 +2,11 @@ namespace Profitocracy.Mobile.Pages;
 
 public partial class CurrencyRatesPage : ContentPage
 {
+	private readonly List<string> _testList = ["", ""];
+	
 	public CurrencyRatesPage()
 	{
 		InitializeComponent();
+		CurrenciesListView.ItemsSource = _testList;
 	}
 }
